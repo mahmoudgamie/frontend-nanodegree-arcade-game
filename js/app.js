a5bb0f62fa8a9d787d60525a8474d06f96d8a348
Mahmoud Gamie <mimg414@gmail.com>
2018-07-02 22:25:00 +0200

    feat: draw main game grid

--- js/app.js
+++ js/app.js
@@ -25,11 +25,33 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(){
+    this.startPosition = 
+    this.boy = 'image/char-boy.png';
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+
+}
+
+Player.prototype.handleInput = function(movement) {
+    console.log(movement);
+    
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [],
+enemy = new Enemy();
+player = new Player();
+allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
